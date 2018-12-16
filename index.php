cf4c00f00efa68f0fe530bd867f758c4c289f8f9
Gaurav Gupta <gauravgupta078619@gmail.com>
2018-12-16 08:15:59 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-<title>G Mailinator</title>
+<title>G Mailinator - An Email Spoofer with Attachments</title>
 <link rel="stylesheet" type="text/css" href="custom.css" />
 <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
